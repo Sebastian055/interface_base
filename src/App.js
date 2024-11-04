2333f82375b8ff82f39484bc7f0b82da56eb9434
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-11-04 13:48:11 -0500

    Taller React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
+import PaginaInicio from './pages/home/Home';
+import PaginaAcercaDe from './pages/home/Home';
+import PaginaContacto from './pages/home/Home';
+import Encabezado from './pages/home/Home';
+import PaginaInicial from './pages/home/Home';
+import './styles/App.cs'
 import Main from './pages/main/Main';
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className='main-screen' >
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='main' element={<Main />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Header />
+      <nav> 
+        <link to ="/"> Inicio</link>
+        <link to ="/about">Acerca de</link>
+        <link to ="/contact">Contacto</link>
+      </nav>
+      <Routes>
+        <Route path ="/" element = {<PaginaInicio />} />
+        <Route path ="/" element = {<PaginaAcercaDe />} />
+        <Route path ="/" element = {<PaginaContacto />} />  
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
